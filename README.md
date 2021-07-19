31d7b8e5f97d5740507468c8a9132a2a348f2635
Samuel Carvalho <samuel.carvalho@monetizze.com.br>
2021-07-18 23:54:36 -0300

    Atualização de arch

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # carcereiro-web
 
 Aplicação para realizar a auto-liberação de acesso a banco de dados. Integrando com o Rocket Chat e o GLPI.
+
+![Fluxo](docs/arch.png)
--- main.go
+++ main.go
@@ -1,22 +1,36 @@
 package main
 
 import (
-	"net/http"
-
-	"github.com/gin-gonic/gin"
+	"fmt"
+	"io/ioutil"
+	"log"
 )
 
+func init() {
+	content, err := ioutil.ReadFile(".config")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(string(content))
+	
+}
+
 func main() {
-	r := gin.Default()
-	r.LoadHTMLGlob("templates/*.tmpl")
 
-	r.GET("/", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "index.tmpl", gin.H{})
-	})
+	/* 	r := gin.Default()
+	   	r.LoadHTMLGlob("templates/*.tmpl")
 
-	r.POST("/gerarcodigo", func(c *gin.Context) {
-		usuario := c.PostForm("usuario")
-		c.String(http.StatusOK, "teste", usuario)
-	})
-	r.Run()
+	   	r.GET("/", func(c *gin.Context) {
+	   		c.HTML(http.StatusOK, "index.tmpl", gin.H{})
+	   	})
+
+	   	r.POST("/gerarcodigo", func(c *gin.Context) {
+	   		usuario := c.PostForm("usuario")
+	   		enviar(usuario)
+	   		c.String(http.StatusOK, "teste", usuario)
+	   	})
+	   	r.Run() */
+
 }
