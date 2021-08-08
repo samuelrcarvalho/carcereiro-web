13e1cf49ea4067b96b5a980ecae7d1e982b6701d
Samuel Carvalho <samuel.carvalho@monetizze.com.br>
2021-08-07 23:26:39 -0300

    Update do sql de criacao do banco

--- install/carcereiro.sql
+++ install/carcereiro.sql
@@ -1,8 +1,8 @@
 -- --------------------------------------------------------
--- Host:                         localhost
+-- Host:                         127.0.0.1
 -- Server version:               5.6.34 - MySQL Community Server (GPL)
 -- Server OS:                    Linux
--- HeidiSQL Version:             11.2.0.6253
+-- HeidiSQL Version:             11.3.0.6295
 -- --------------------------------------------------------
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
@@ -17,20 +17,57 @@
 CREATE DATABASE IF NOT EXISTS `carcereiro` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci */;
 USE `carcereiro`;
 
--- Dumping structure for table carcereiro.config
-CREATE TABLE IF NOT EXISTS `config` (
-  `config` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+-- Dumping structure for table carcereiro.auths
+CREATE TABLE IF NOT EXISTS `auths` (
+  `user` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `authcode` varchar(6) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `expire_in` datetime DEFAULT NULL,
+  PRIMARY KEY (`user`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- Dumping structure for table carcereiro.configs
+CREATE TABLE IF NOT EXISTS `configs` (
+  `config` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
   `value` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   PRIMARY KEY (`config`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
--- Dumping data for table carcereiro.config: ~0 rows (approximately)
-/*!40000 ALTER TABLE `config` DISABLE KEYS */;
-INSERT INTO `config` (`config`, `value`) VALUES
-	('rocket_integracao_auth_token', 'token'),
-	('rocket_integracao_user_id', 'user'),
-	('rocket_url', 'url');
-/*!40000 ALTER TABLE `config` ENABLE KEYS */;
+-- Dumping data for table carcereiro.configs: ~14 rows (approximately)
+/*!40000 ALTER TABLE `configs` DISABLE KEYS */;
+INSERT INTO `configs` (`config`, `value`) VALUES
+	('glpi_categoryid_autoapproved', ''),
+	('glpi_categoryid_toapprove', ''),
+	('glpi_integracao_app-token', ''),
+	('glpi_integracao_authorization', ''),
+	('glpi_requesttypeid', ''),
+	('glpi_url', ''),
+	('rocket_integracao_auth_token', ''),
+	('rocket_integracao_user_id', ''),
+	('rocket_url', ''),
+	('target_database_host', ''),
+	('target_database_port', ''),
+	('target_database_pwd', ''),
+	('target_database_schema', ''),
+	('target_database_user', '');
+/*!40000 ALTER TABLE `configs` ENABLE KEYS */;
+
+-- Dumping structure for table carcereiro.importanttables
+CREATE TABLE IF NOT EXISTS `importanttables` (
+  `nome` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`nome`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- Dumping structure for table carcereiro.tables
+CREATE TABLE IF NOT EXISTS `tables` (
+  `nome` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`nome`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- Dumping structure for table carcereiro.users_excepts
+CREATE TABLE IF NOT EXISTS `users_excepts` (
+  `user` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`user`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
